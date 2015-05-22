85d4da2a1466001d7bc900037b9e6d4d5c47f92c
godrose <godrose@gmail.com>
2015-05-22 22:25:56 +0300

    Added TearDown impl for integration c.micro related tests

--- LogoFX.UI.Tests.Core/TestsBase.cs
+++ LogoFX.UI.Tests.Core/TestsBase.cs
@@ -1,12 +1,12 @@
 ﻿using Attest.Fake.Core;
-using Attest.Tests.Core;
+using Attest.Tests.NUnit;
 using Caliburn.Micro;
 using Solid.Practices.IoC;
 
 namespace LogoFX.UI.Tests.Core
 {
-    public abstract class TestsBase<TContainer,TFakeFactory,TRootViewModel> : 
-        IntegrationTestsBase<TContainer,TFakeFactory,TRootViewModel> 
+    public abstract class TestsBase<TContainer,TFakeFactory,TRootViewModel, TBootstrapper> : 
+        IntegrationTestsBase<TContainer,TFakeFactory,TRootViewModel, TBootstrapper> 
         where TContainer : IIocContainer, new() 
         where TFakeFactory : IFakeFactory, new() 
         where TRootViewModel : class
@@ -15,6 +15,12 @@ namespace LogoFX.UI.Tests.Core
         {
             ScreenExtensions.TryActivate(rootObject);
             return rootObject;
+        }
+
+        protected override void TearDownOverride()
+        {
+            base.TearDownOverride();
+            AssemblySource.Instance.Clear();
         }
     }
 }
