ae4e6950dcd122294af1f826659f1fffa61c0f6f
godrose <godrose@gmail.com>
2015-06-18 17:28:43 +0300

    Added ModelsSelection

---
+++ LogoFX.UI.ViewModels/Services/ModelsSelection.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LogoFX.UI.ViewModels.Services
+{
+    public interface IReadModelsSelection<out TItem>
+    {
+        IEnumerable<TItem> Selection { get; }
+        event EventHandler SelectionChanged;
+    }
+
+    public interface IWriteModelsSelection<in TItem>
+    {
+        void UpdateSelection(IEnumerable<TItem> newSelection);
+    }
+
+    public class ModelsSelection<TItem> : IReadModelsSelection<TItem>, IWriteModelsSelection<TItem>
+    {
+        public IEnumerable<TItem> Selection { get; private set; }
+        public event EventHandler SelectionChanged;
+        public void UpdateSelection(IEnumerable<TItem> newSelection)
+        {
+            Selection = newSelection;
+            if (SelectionChanged != null)
+            {
+                SelectionChanged(this, new EventArgs());
+            }
+        }
+    }
+}
