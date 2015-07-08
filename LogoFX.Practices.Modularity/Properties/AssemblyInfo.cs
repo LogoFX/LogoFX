a0c4787dc3d1656b99a46d1b572f8593310078bf
godrose <godrose@gmail.com>
2015-07-08 10:26:40 +0300

    Renamed composition to modularity inside practices

--- LogoFX.Practices.Modularity/Properties/AssemblyInfo.cs
+++ LogoFX.Practices.Modularity/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("LogoFX.Practices.Composition")]
+[assembly: AssemblyTitle("LogoFX.Practices.Modularity")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("LogoFX.Practices.Composition")]
+[assembly: AssemblyProduct("LogoFX.Practices.Modularity")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
