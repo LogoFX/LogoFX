562f0ad3b3f5acd4787621c919494607fa6d5bf1
godrose <godrose@gmail.com>
2015-07-12 17:13:45 +0300

    Fixed output path for release mode

--- LogoFX.Client.Mvvm.View.Infra/Properties/AssemblyInfo.cs
+++ LogoFX.Client.Mvvm.View.Infra/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("LogoFX.Client.UI.View.Infra")]
+[assembly: AssemblyTitle("LogoFX.Client.Mvvm.View.Infra")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("LogoFX.Client.UI.View.Infra")]
+[assembly: AssemblyProduct("LogoFX.Client.Mvvm.View.Infra")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
