f58b82c5965b9c8270305f7f722509c961644e27
godrose <godrose@gmail.com>
2015-07-30 15:43:44 +0300

    Added dependencies collection test

--- LogoFX.Practices.IoC.Tests/ExtendedSimpleContainerTests.cs
+++ LogoFX.Practices.IoC.Tests/ExtendedSimpleContainerTests.cs
@@ -19,5 +19,21 @@ namespace LogoFX.Practices.IoC.Tests
             var actualModel = dependency.Model;
             Assert.AreEqual(model, actualModel);
         }
+
+        [Test]
+        public void
+            GivenThereAreMultipleSameTypedDependencies_WhenDependencyIsRegisteredByHandlerAndDependencyIsRegisteredByHandler_ThenResolutionOfDependenciesCollectionIsCorrect
+            ()
+        {
+            var modules = new ITestModule[] {new TestModule {Name = "1"}, new TestModule {Name = "2"}};
+
+            var container = new ExtendedSimpleContainer();
+            container.RegisterHandler(typeof(ITestModule), null,(c,r) => modules[0]);
+            container.RegisterHandler(typeof(ITestModule), null, (c, r) => modules[1]);
+            var actualModules = container.GetAllInstances(typeof (ITestModule));
+
+            CollectionAssert.AreEqual(modules, actualModules);
+        }
+
     }
 }
---
+++ LogoFX.Practices.IoC.Tests/TestModule.cs
@@ -0,0 +1,12 @@
+﻿namespace LogoFX.Practices.IoC.Tests
+{
+    interface ITestModule
+    {
+        string Name { get; set; }
+    }
+
+    class TestModule : ITestModule
+    {
+        public string Name { get; set; }
+    }
+}
