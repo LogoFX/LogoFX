1a1d33a807c5421bf99a10d987d9b92bf52915bc
godrose <godrose@gmail.com>
2015-08-10 17:09:30 +0300

    Minor

---
+++ LogoFX.Client.Mvvm.Model.Tests/CompositeModelDirtyTests.cs
@@ -0,0 +1,62 @@
+using NUnit.Framework;
+
+namespace LogoFX.Client.Mvvm.Model.Tests
+{
+    [TestFixture]
+    class CompositeModelDirtyTests
+    {
+        [Test]
+        public void InnerModelIsNotMadeDirty_IsDirtyIsFalse()
+        {
+            var compositeModel = new CompositeEditableModel("location");            
+
+            Assert.IsFalse(compositeModel.IsDirty);
+        }
+
+        [Test]
+        public void InnerModelIsMadeDirty_IsDirtyIsTrue()
+        {
+            var compositeModel = new CompositeEditableModel("location");
+            compositeModel.Person.Name = DataGenerator.InvalidName;
+
+            Assert.IsTrue(compositeModel.IsDirty);
+        }
+
+        [Test]
+        public void InnerModelIsReset_DirtyNotificationIsRaised()
+        {
+            var compositeModel = new CompositeEditableModel("location");
+            var isRaised = false;
+            compositeModel.PropertyChanged += (sender, args) =>
+            {
+                if (args.PropertyName == "IsDirty")
+                {
+                    isRaised = true;
+                }
+            };
+            compositeModel.Person = new SimpleEditableModel(DataGenerator.ValidName, 0);
+
+            Assert.IsTrue(isRaised);
+        }
+
+        [Test]
+        public void InnerModelIsMadeDirtyThenClearDirtyIsCalledWithChildrenEnforcement_IsDirtyIsFalse()
+        {
+            var compositeModel = new CompositeEditableModel("location");
+            compositeModel.Person.Name = DataGenerator.InvalidName;
+            compositeModel.ClearDirty(forceClearChildren:true);
+
+            Assert.IsFalse(compositeModel.IsDirty);
+        }
+
+        [Test]
+        public void InnerModelIsMadeDirtyThenClearDirtyIsCalledWithoutChildrenEnforcement_IsDirtyIsTrue()
+        {
+            var compositeModel = new CompositeEditableModel("location");
+            compositeModel.Person.Name = DataGenerator.InvalidName;
+            compositeModel.ClearDirty(forceClearChildren: false);
+
+            Assert.IsTrue(compositeModel.IsDirty);
+        }
+    }
+}
