5b27f6f24968fad6204450ba5e9c5456764dc636
godrose <godrose@gmail.com>
2015-08-27 16:57:38 +0300

    Removed ModelsCollection (moved to LogoFX.Client.Mvvm.Model package)

--- LogoFX.Client.Mvvm.ViewModel.Interfaces/Services/IModelsCollection.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace LogoFX.Client.Mvvm.ViewModel.Interfaces.Services
-{
-    public interface IModelsCollection<TItem> : IReadModelsCollection<TItem>, IInfoModelsCollection,
-        IWriteModelsCollection<TItem>
-    {
-        
-    }
-}
--- LogoFX.Client.Mvvm.ViewModel.Interfaces/Services/IReadModelsCollection.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace LogoFX.Client.Mvvm.ViewModel.Interfaces.Services
-{
-    public interface IReadModelsCollection<TItem>
-    {
-        IEnumerable<TItem> Items { get; }
-    }
-}
