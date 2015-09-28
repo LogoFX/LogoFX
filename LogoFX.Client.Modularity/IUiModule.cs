86fcf7c293c7e77c0e54671a74a30e44b1d5d770
godrose <godrose@gmail.com>
2015-09-28 22:23:39 +0300

    Added comments to LogoFX.Client.Modularity package

--- LogoFX.Client.Modularity/IUiModule.cs
+++ LogoFX.Client.Modularity/IUiModule.cs
@@ -3,19 +3,41 @@ using Solid.Practices.Modularity;
 
 namespace LogoFX.Client.Modularity
 {
+    /// <summary>
+    /// Represents user interface composition module
+    /// </summary>
     public interface IUiModule : ICompositionModule
     {
+        /// <summary>
+        /// Module ID
+        /// </summary>
         string Id { get; }
 
+        /// <summary>
+        /// Module display name
+        /// </summary>
         string Name { get; }
 
+        /// <summary>
+        /// Module display order
+        /// </summary>
         int Order { get; }                
     }
 
+    /// <summary>
+    /// Respresents user interface composition module with root view model
+    /// </summary>
+    /// <typeparam name="TRootViewModel">Type of Root ViewModel</typeparam>
     public interface IUiModule<out TRootViewModel> : IUiModule
     {
+        /// <summary>
+        /// Root ViewModel
+        /// </summary>
         TRootViewModel RootViewModel { get; }
 
+        /// <summary>
+        /// Explicit type of Root ViewModel
+        /// </summary>
         Type RootModelType { get; }
     }
 }
