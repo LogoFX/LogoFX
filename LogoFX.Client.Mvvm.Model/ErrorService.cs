398e925fd8f54ba7b93241c25cc794b2d9692b2a
godrose <godrose@gmail.com>
2015-10-04 21:56:11 +0300

    Added method for getting collection of validation errors by property name

--- LogoFX.Client.Mvvm.Model/ErrorService.cs
+++ LogoFX.Client.Mvvm.Model/ErrorService.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Reflection;
 using System.Text;
 
 namespace LogoFX.Client.Mvvm.Model
@@ -10,7 +12,7 @@ namespace LogoFX.Client.Mvvm.Model
     public static class ErrorService
     {
         /// <summary>
-        /// Gets validation errors by property name
+        /// Gets validation errors description by property name
         /// </summary>
         /// <param name="type">Type of validated object</param>
         /// <param name="propertyName">Property name</param>
@@ -18,12 +20,47 @@ namespace LogoFX.Client.Mvvm.Model
         /// <returns>Textual representation of errors, if any; empty string otherwise</returns>
         public static string GetValidationErrorByPropertyName(Type type, string propertyName, object propertyContainer)
         {
+            var validationErrors = GetValidationErrorsByPropertyNameInternal(type, propertyName, propertyContainer);
+            if (validationErrors == null)
+            {
+                return null;
+            }            
+            var stringBuilder = new StringBuilder();            
+            foreach (var validationError in validationErrors)
+            {
+                stringBuilder.Append(validationError);
+            }
+            return stringBuilder.ToString();
+        }
+
+        /// <summary>
+        /// Gets validation errors collection by property name
+        /// </summary>
+        /// <param name="type">Type of validated object</param>
+        /// <param name="propertyName">Property name</param>
+        /// <param name="propertyContainer">Validated object</param>
+        /// <returns>Collection of validation errors</returns>
+        public static IEnumerable<string> GetValidationErrorsByPropertyName(Type type, string propertyName, object propertyContainer)
+        {
+            return GetValidationErrorsByPropertyNameInternal(type, propertyName, propertyContainer);
+        }
+
+        private static IEnumerable<string> GetValidationErrorsByPropertyNameInternal(Type type, string propertyName,
+            object propertyContainer)
+        {
             var validationInfo = TypeInformationProvider.GetValidationInfo(type, propertyName);
             if (validationInfo == null)
             {
                 return null;
             }
-            var stringBuilder = new StringBuilder();            
+            return GetValidationErrorsByPropertyNameFromValidationInfo(type, propertyName, propertyContainer,
+                validationInfo);
+        }
+
+        private static IEnumerable<string> GetValidationErrorsByPropertyNameFromValidationInfo(
+            Type type, string propertyName, object propertyContainer,
+            Tuple<PropertyInfo, ValidationAttribute[]> validationInfo)
+        {
             foreach (var validationAttribute in validationInfo.Item2)
             {
                 var validationInfoValue = TypeInformationProvider.GetValidationInfoValue(type, propertyName,
@@ -33,10 +70,9 @@ namespace LogoFX.Client.Mvvm.Model
                         new ValidationContext(propertyName));
                 if (validationResult != null)
                 {
-                    stringBuilder.Append(validationResult.ErrorMessage);
+                    yield return validationResult.ErrorMessage;
                 }
-            }
-            return stringBuilder.ToString();
+            }   
         }
     }
 }
