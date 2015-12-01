3477126ab5a74a2def5e2e16c420ddd4374b4c42
godrose <godrose@gmail.com>
2015-12-01 10:37:21 +0200

    Updated nuget packages

---
+++ LogoFX.Client.Tests.Specflow/EndToEndTestsBase.cs
@@ -0,0 +1,15 @@
+﻿using Attest.Fake.Core;
+using Solid.Practices.IoC;
+
+namespace LogoFX.Client.Tests.SpecFlow
+{
+    public abstract class EndToEndTestsBase<TContainer, TFakeFactory> :
+        Attest.Tests.SpecFlow.EndToEndTestsBase<TContainer, TFakeFactory>
+        where TContainer : IIocContainer, new()
+        where TFakeFactory : IFakeFactory, new()
+    {
+        protected EndToEndTestsBase(TContainer container) : base(container)
+        {
+        }
+    }
+}
--- LogoFX.Client.Tests.Specflow/IntegrationTestsBase.cs
+++ LogoFX.Client.Tests.Specflow/IntegrationTestsBase.cs
@@ -1,20 +1,19 @@
 ﻿using Attest.Fake.Core;
 using Attest.Tests.Core;
-using Attest.Tests.SpecFlow;
 using LogoFX.Client.Tests.Shared;
 using Solid.Practices.IoC;
 
 namespace LogoFX.Client.Tests.SpecFlow
 {
-    public abstract class TestsBase<TContainer, TFakeFactory, TRootViewModel, TBootstrapper> :
-        IntegrationTestsBase<TContainer, TFakeFactory, TRootViewModel, TBootstrapper>
+    public abstract class IntegrationTestsBase<TContainer, TFakeFactory, TRootViewModel, TBootstrapper> : 
+        Attest.Tests.SpecFlow.IntegrationTestsBase<TContainer, TFakeFactory, TRootViewModel, TBootstrapper>
         where TContainer : IIocContainer, new()
         where TFakeFactory : IFakeFactory, new()
         where TRootViewModel : class
     {
         private readonly InitializationParametersResolutionStyle _resolutionStyle;
 
-        protected TestsBase(InitializationParametersResolutionStyle resolutionStyle = InitializationParametersResolutionStyle.PerRequest)
+        protected IntegrationTestsBase(InitializationParametersResolutionStyle resolutionStyle = InitializationParametersResolutionStyle.PerRequest)
             :base(resolutionStyle)
         {
             _resolutionStyle = resolutionStyle;
