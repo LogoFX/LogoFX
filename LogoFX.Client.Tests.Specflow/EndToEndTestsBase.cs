f7dd147fd09dfb8b51c92b2ce72f510526eab153
godrose <godrose@gmail.com>
2016-01-19 09:28:26 +0200

    Added compatibility to the latest Attest.Tests.* packages.

--- LogoFX.Client.Tests.Specflow/EndToEndTestsBase.cs
+++ LogoFX.Client.Tests.Specflow/EndToEndTestsBase.cs
@@ -1,21 +1,10 @@
-﻿using Solid.Practices.IoC;
-
-namespace LogoFX.Client.Tests.SpecFlow
+﻿namespace LogoFX.Client.Tests.SpecFlow
 {
     /// <summary>
     /// Base class for client end-to-end tests.
-    /// </summary>
-    /// <typeparam name="TContainer"></typeparam>
-    public abstract class EndToEndTestsBase<TContainer> :
-        Attest.Tests.SpecFlow.EndToEndTestsBase<TContainer>
-        where TContainer : IIocContainer, new()
+    /// </summary>    
+    public abstract class EndToEndTestsBase :
+        Attest.Tests.SpecFlow.EndToEndTestsBase        
     {
-        /// <summary>
-        /// Initializes a new instance of the <see cref="IntegrationTestsBase{TContainer, TRootViewModel, TBootstrapper}"/> class.
-        /// </summary>
-        /// <param name="container">The container.</param>
-        protected EndToEndTestsBase(TContainer container) : base(container)
-        {
-        }
     }
 }
