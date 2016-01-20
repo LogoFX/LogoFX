acff491444ff06ea833d43f63d63f78ac27bb74d
godrose <godrose@gmail.com>
2016-01-20 14:18:14 +0200

    Added default functionality to the base class for end-to-end tests - usually used inside the bridge hook.

--- LogoFX.Client.Tests.Specflow/EndToEndTestsBase.cs
+++ LogoFX.Client.Tests.Specflow/EndToEndTestsBase.cs
@@ -1,4 +1,9 @@
-﻿namespace LogoFX.Client.Tests.SpecFlow
+﻿using Attest.Tests.Core;
+using LogoFX.Client.Tests.Contracts;
+using LogoFX.Client.Tests.EndToEnd;
+using LogoFX.Client.Tests.EndToEnd.White;
+
+namespace LogoFX.Client.Tests.SpecFlow
 {
     /// <summary>
     /// Base class for client end-to-end tests.
@@ -6,5 +11,40 @@
     public abstract class EndToEndTestsBase :
         Attest.Tests.SpecFlow.EndToEndTestsBase        
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EndToEndTestsBase"/> class.
+        /// </summary>
+        protected EndToEndTestsBase()
+        {
+            ScenarioHelper.Add(new StartApplicationService(), typeof(IStartApplicationService));
+            ScenarioHelper.Add(new BuilderRegistrationService(), typeof(IBuilderRegistrationService));
+            RegisterScreenObjectsCore();
+        }
+
+        private void RegisterScreenObjectsCore()
+        {
+            RegisterScreenObjects();
+        }
+
+        /// <summary>
+        /// Override this method to register the screen objects.
+        /// </summary>
+        protected virtual void RegisterScreenObjects()
+        {
+            
+        }
+
+        /// <summary>
+        /// Called when the teardown finishes
+        /// </summary>
+        protected override void OnAfterTeardown()
+        {
+            base.OnAfterTeardown();
+            if (ApplicationContext.Application != null)
+            {
+                ApplicationContext.Application.Dispose();
+            }
+            ScenarioHelper.Clear();
+        }
     }
 }
