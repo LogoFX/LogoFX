7f1625a74ce856d33d7e58cb68de9bc31e6772a2
godrose <godrose@gmail.com>
2016-02-01 14:36:37 +0200

    Updated Attest packages.

--- LogoFX.Client.Tests.Integration.xUnit/BuilderRegistrationService.cs
+++ LogoFX.Client.Tests.Integration.xUnit/BuilderRegistrationService.cs
@@ -6,9 +6,9 @@ namespace LogoFX.Client.Tests.Integration.xUnit
     /// <summary>
     /// Represents builder registration service for NUnit-based integration tests.
     /// </summary>
-    /// <seealso cref="Attest.Tests.NUnit.StepsBase" />
+    /// <seealso cref="Attest.Tests.xUnit.StepsBase" />
     /// <seealso cref="IBuilderRegistrationService" />
-    public class BuilderRegistrationService : Attest.Tests.NUnit.StepsBase, IBuilderRegistrationService
+    public class BuilderRegistrationService : Attest.Tests.xUnit.StepsBase, IBuilderRegistrationService
     {
         void IBuilderRegistrationService.RegisterBuilder<TService>(FakeBuilderBase<TService> builder)
         {
