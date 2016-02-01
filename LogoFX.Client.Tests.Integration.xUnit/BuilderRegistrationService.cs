0c8de22d273d734db5b82f8b5ece85fdc8f24a29
godrose <godrose@gmail.com>
2016-02-01 13:54:54 +0200

    Added Client Integration Tests using xUnit.

---
+++ LogoFX.Client.Tests.Integration.xUnit/BuilderRegistrationService.cs
@@ -0,0 +1,18 @@
+﻿using Attest.Fake.Builders;
+using LogoFX.Client.Tests.Contracts;
+
+namespace LogoFX.Client.Tests.Integration.xUnit
+{
+    /// <summary>
+    /// Represents builder registration service for NUnit-based integration tests.
+    /// </summary>
+    /// <seealso cref="Attest.Tests.NUnit.StepsBase" />
+    /// <seealso cref="IBuilderRegistrationService" />
+    public class BuilderRegistrationService : Attest.Tests.NUnit.StepsBase, IBuilderRegistrationService
+    {
+        void IBuilderRegistrationService.RegisterBuilder<TService>(FakeBuilderBase<TService> builder)
+        {
+            RegisterBuilder(builder);
+        }
+    }
+}
