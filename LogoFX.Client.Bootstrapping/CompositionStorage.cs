88bedfd25f3762c64969bc3d2adfb602f991cb02
godrose <godrose@gmail.com>
2016-03-09 10:33:41 +0200

    Added missing file

---
+++ LogoFX.Client.Bootstrapping/CompositionStorage.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Concurrent;
+using Solid.Practices.Modularity;
+
+namespace LogoFX.Client.Bootstrapping
+{
+    static class CompositionStorage
+    {
+        private static readonly ConcurrentDictionary<string, ICompositionModule[]> _internalStorage =
+            new ConcurrentDictionary<string, ICompositionModule[]>();
+
+        internal static void AddCompositionModules(string rootPath, ICompositionModule[] compositionModules)
+        {
+            _internalStorage.TryAdd(rootPath, compositionModules);
+        }
+
+        internal static ICompositionModule[] GetCompositionModules(string rootPath)
+        {
+            ICompositionModule[] compositionModules;
+            _internalStorage.TryGetValue(rootPath, out compositionModules);
+            return compositionModules;
+        }
+    }
+}
